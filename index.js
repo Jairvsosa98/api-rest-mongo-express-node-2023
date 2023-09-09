0313ded10051a3671a194806345f0aff0e900d7d
Jairvsosa98 <jairvillazonsosa98@outlook.com>
2023-09-09 12:15:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.use('/', redirectRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/links', linkRouter);
 // solo para el ejemplo de login/token
-app.use(express.static('public'));
+// app.use(express.static('public'));
 const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => console.log("❤❤❤ http://localhost:" + PORT));
