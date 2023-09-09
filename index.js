eb44cee84475708f0c46b14a87eabb777b537978
Jairvsosa98 <jairvillazonsosa98@outlook.com>
2023-09-09 12:01:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const whiteList = [process.env.ORIGIN2];
 
 app.use(cors({
     origin: function (origin, callback) {
-        if (whiteList.includes(origin)) {
+        console.log("😮😮😮 => " + origin );
+        if (!origin||whiteList.includes(origin)) {
             return callback(null, origin);
         }
         return callback("Error de CORS origin: " + origin + " no autorizado!");
